749e9f183de8fce07b748826ce7a6d9ae2737950
Annu Poonia <annupoonia086@gmail.com>
2025-04-16 13:45:51 +0530

    Data Fetched

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -6,23 +6,23 @@ function Footer() {
     <footer className="w-full bg-cold-blue flex items-center justify-center text-white shadow p-4 lg:px-6 lg:py-8 ">
       <div className="flex flex-col items-center justify-center w-full">
         <div className="w-full flex items-center justify-center sm:justify-end h-[100px] pr-2">
-          <ul className="flex flex-col sm:flex-row flex-wrap items-center !justify-center md:justify-start text-white sm:mr-4 text-base sm:text-lg space-y-1 sm:space-x-6 ">
+          <ul className="flex flex-col sm:flex-row flex-wrap items-center justify-center md:justify-start text-white sm:mr-4 text-base sm:text-lg space-y-1 sm:space-y-0 sm:space-x-6 ">
             <Link to="/chart">
               <li className="hover:underline">Chart</li>
             </Link>
-            <a href="/">
+            <Link to="/">
               <li className="hover:underline">Privacy Policy</li>
-            </a>
-            <a href="/">
+            </Link>
+            <Link to="/">
               <li className="hover:underline">Terms & Conditions</li>
-            </a>
-            <a
-              href="https://wa.me/+917015916793"
+            </Link>
+            <Link
+              to="https://wa.me/+917015916793"
               target="_blank"
               rel="noopener noreferrer"
             >
               <li className="hover:underline">Connect</li>
-            </a>
+            </Link>
           </ul>
         </div>
         <div className="w-full flex items-center justify-center border-y border-lighter-gray mt-4 lg:mt-8 md:pt-8 p-4 md:pb-4 mb-4">
